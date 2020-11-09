e6d8547ab871415dc65b1de84e41ff9baff142c0
Marco Comparato <marcocom@gmail.com>
2020-11-09 12:45:57 -0800

    WorkHero refactored to new Welcome module

--- src/pages/mywork.jsx
+++ src/pages/mywork.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Layout } from '@src/ui/layout'
-import { WorkHero, Work } from '@src/modules/work'
+import { Work } from '@src/modules/work'
 
 const Mywork = () => (
   <Layout pageTitle='My Work'>
